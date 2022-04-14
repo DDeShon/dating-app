6c3487c475903e41b8de271b2d37c5f226361e27
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-04-14 09:27:04 -0500

    added dependencies for server function

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const PORT = 8000;
 
 const express = require("express");
+const { MongoClient } = require("mongodb");
 
 const app = express();
 
 app.get("/", (req, res) => {
+  res.json("Welcome to my app");
+});
+
+app.get("/signup", (req, res) => {
   res.json("Welcome to my app");
 });
 
