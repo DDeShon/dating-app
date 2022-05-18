11635ab7309e20fdb9bab637e5d10002b6eeefef
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-05-18 09:47:33 -0500

    working on matches

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.production.min";
+
 const MatchesDisplay = ({ matches }) => {
+  const [matchedProfiles, setMatchedProfiles] = useState(null);
+  const matchedUserIds = matches.map(({ user_id }) => user_id);
+
+  const getMatches = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/users", {
+        params: { userIds: JSON.stringify(matchedUserIds) },
+      });
+      setMatchedProfiles(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMatches();
+  }, []);
+
+  console.log(matchedProfiles);
+
   return <div className="matches-display">Matches</div>;
 };
 
