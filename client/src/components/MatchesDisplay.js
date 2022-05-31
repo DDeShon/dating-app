5110b6a934154ccaab92758aff42b74a31fa9d24
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-05-30 22:39:02 -0500

    working on matches

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -30,6 +30,7 @@ const MatchesDisplay = ({ matches }) => {
           <div className="img-container">
             <img src={match?.url} alt={match?.first_name + "profile"} />
           </div>
+          <h3>{match?.first_name}</h3>
         </div>
       ))}
     </div>
