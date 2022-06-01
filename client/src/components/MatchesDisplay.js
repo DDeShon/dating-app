8704d2b043c985475f82a8694648b26de3025b5f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-05-31 23:03:01 -0500

    working on matches

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -28,7 +28,7 @@ const MatchesDisplay = ({ matches }) => {
       {matchedProfiles?.map((match, _index) => (
         <div key={{ _index }} className="match-card">
           <div className="img-container">
-            <img src={match?.url} alt={match?.first_name + "profile"} />
+            <img src={match?.url} alt={match?.first_name + " profile"} />
           </div>
           <h3>{match?.first_name}</h3>
         </div>
