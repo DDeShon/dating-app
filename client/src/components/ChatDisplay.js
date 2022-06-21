3c398175e83408b169edc9e0d61b245a4b51b57e
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-06-21 09:48:50 -0500

    working on chat function

--- client/src/components/ChatDisplay.js
+++ client/src/components/ChatDisplay.js
@@ -38,6 +38,8 @@ const ChatDisplay = ({ user, clickedUser }) => {
 
   const messages = [];
 
+  console.log("usersMessages", usersMessages);
+
   usersMessages?.forEach((message) => {
     const formattedMessage = {};
     formattedMessage["name"] = user?.first_name;
@@ -55,6 +57,9 @@ const ChatDisplay = ({ user, clickedUser }) => {
     formattedMessage["timestamp"] = message.timestamp;
     messages.push(formattedMessage);
   });
+
+  console.log("usersMessages", usersMessages);
+  console.log("formattedMessage", messages);
 
   const descendingOrderMessages = messages?.sort((a, b) =>
     a.timestamp.localeCompare(b.timestamp)
