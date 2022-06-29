5524204bfcb76fda82fac54759ee1013b9625b81
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-06-28 23:09:56 -0500

    working on chat function

--- client/src/components/ChatInput.js
+++ client/src/components/ChatInput.js
@@ -7,6 +7,17 @@ const ChatInput = ({
   getClickedUsersMessages,
 }) => {
   const [textArea, setTextArea] = useState(null);
+  const userId = user?.user_id;
+  const clickedUserId = clickedUser?.user_id;
+
+  const addMessage = async () {
+    const message = {
+      timestamp: new Date().toISOString(),
+      from_userId: userId,
+      to_userId: clickedUserId,
+      message: textArea
+    }
+  }
 
   return (
     <div className="chat-input">
